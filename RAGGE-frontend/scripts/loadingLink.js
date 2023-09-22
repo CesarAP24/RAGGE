338aa0f73bb5fb3b7aafb3173786b4dc458ab1c8
CesarAP24 <cesar.perales@utec.edu.pe>
2023-09-22 14:49:52 -0500

    loading, mis cursos page, more css. frontend

---
+++ RAGGE-frontend/scripts/loadingLink.js
@@ -0,0 +1,31 @@
+// Función para mostrar la pantalla de carga
+function mostrarPantallaCarga(destino) {
+    var pantallaCarga = document.getElementById("pantalla-carga");
+    pantallaCarga.style.display = "flex";
+
+    setTimeout(function() {
+        // Redirigir al destino después de 2 segundos
+        window.location.href = destino;
+    }, 10); // 2000 milisegundos (2 segundos)
+}
+
+
+// Función para ocultar la pantalla de carga
+function ocultarPantallaCarga() {
+    var pantallaCarga = document.getElementById("pantalla-carga");
+    pantallaCarga.style.display = "none";
+}
+
+// Agrega un evento de clic a todos los enlaces que muestre la pantalla de carga
+var enlaces = document.querySelectorAll("a");
+
+for (var i = 0; i < enlaces.length; i++) {
+    enlaces[i].addEventListener("click", function(event) {
+        event.preventDefault(); // Evita la navegación predeterminada
+        var destino = this.getAttribute("href");
+        mostrarPantallaCarga(destino);
+    });
+}
+
+window.addEventListener("load", ocultarPantallaCarga);
+
